1e6ec2385e34dd2e3177d025e9548f0c3c143b1e
Travis CI User <travis@example.org>
2020-01-18 00:53:55 +0000

    Documentation for Amanieu/intrusive-rs@d44814a

---
+++ implementors/core/marker/trait.StructuralEq.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["intrusive_collections"] = [{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"enum\" href=\"intrusive_collections/enum.Bound.html\" title=\"enum intrusive_collections::Bound\">Bound</a>&lt;T&gt;","synthetic":false,"types":["intrusive_collections::Bound"]}];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["intrusive_collections"] = {"name":"","files":["adapter.rs","intrusive_pointer.rs","key_adapter.rs","lib.rs","linked_list.rs","rbtree.rs","singly_linked_list.rs","unsafe_ref.rs","xor_linked_list.rs"]};
+sourcesIndex["memoffset"] = {"name":"","files":["lib.rs","offset_of.rs","span_of.rs"]};
+createSourceSidebar();
